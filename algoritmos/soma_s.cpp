bde0cc3ae9d70920f0e439456ecbdea7e90c89c6
Louise Chacon <louisechacon26@gmail.com>
2026-04-20 19:17:45 -0300

    Corrige código da questão soma_s

--- algoritmos/soma_s.cpp
+++ algoritmos/soma_s.cpp
@@ -15,7 +15,7 @@ int main() {
 	int contador = 0;
 	
 	for (int i = 0; i < n; i++) {
-		for (int j = 1; j < n; j++) {
+		for (int j = i + 1; j < n; j++) {
 			if (arr[i] + arr[j] == s) {
 				contador += 1;
 			}
